dfb50bb1bfbd242ee992b11dd1a6d05a040ce663
amorntap.pant@bumail.net <>
2026-09-08 14:15:43 +0700

    complet

--- Assets/Scripts/Workspace/Assignment/Lecture.cs
+++ Assets/Scripts/Workspace/Assignment/Lecture.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using System.Collections;
 
 namespace Assignment
 {
@@ -10,7 +12,7 @@ namespace Assignment
             // LCT01_SyntaxList();
             // LCT02_SyntaxLinkedList();
             // LCT03_SyntaxHashTable();
-            // LCT04_SyntaxDictionary();
+             LCT04_SyntaxDictionary();
         }
 
         #region Lecture
@@ -22,17 +24,87 @@ namespace Assignment
 
         public void LCT02_SyntaxLinkedList()
         {
-            throw new System.NotImplementedException();
+            //throw new System.NotImplementedException();
+            LinkedList<string> linkedlist = new LinkedList<string>();
+
+            linkedlist.AddLast("Node 1");
+            linkedlist.AddLast("Node 2");
+            linkedlist.AddFirst("Node 0");
+
+            LinkedListNode<string> node1 = linkedlist.Find("Node 1");
+            Debug.Log(node1.Value);
+            Debug.Log(node1.Next.Value);
+            Debug.Log(node1.Previous.Value);
+
+            Debug.Log("......");
+
+            var firstNode = linkedlist.First;
+            var lastNode = linkedlist.Last;
+            Debug.Log(firstNode.Previous);
+            Debug.Log(lastNode.Next);
+
+            linkedlist.AddAfter(node1, "Node 1.5");
+            linkedlist.AddBefore(node1, "Node 0.5");
+
+            linkedlist.RemoveFirst();
+            linkedlist.RemoveLast();
+            linkedlist.Remove("1.5");
+
+            linkedlist.Clear();
+
+            foreach (var item in linkedlist)
+            {
+                Debug.Log(item);
+            }
         }
 
         public void LCT03_SyntaxHashTable()
         {
-            throw new System.NotImplementedException();
+            //throw new System.NotImplementedException();
+            Hashtable table = new Hashtable();
+            table.Add("Potion", 5);
+            table.Add(5, "Potion");
+
+            //foreach(var item in table)
+            {
+                //Debug.Log($"item"(item));
+            }
         }
 
         public void LCT04_SyntaxDictionary()
         {
-            throw new System.NotImplementedException();
+            //throw new System.NotImplementedException();
+            Dictionary<string, int> inv = new Dictionary<string, int>();
+
+            inv.Add("Potion", 5);
+            inv.Add("Banana", 1);
+            inv.Add("Apple", 10);
+
+
+            inv["Apple"] = 0;
+
+            inv["Apple1"] = 1;
+
+            int potion = inv["Potion"];
+            Debug.Log("potion:"+ potion);
+
+            //int apple2 = inv["Apple2"];
+            //Debug.Log("apple2:" + apple2);
+
+            bool hasPotion = inv.ContainsKey("Potion");
+            Debug.Log("hasPotion:" + hasPotion);
+
+            inv.Remove("Banana");
+
+            foreach(KeyValuePair<string, int> kvp in inv)
+            {
+                var key = kvp.Key;
+                var value = kvp.Value;
+                Debug.Log($"{key} => {value}");
+                
+            }
+            
+            inv.Clear();
         }
 
         #endregion
